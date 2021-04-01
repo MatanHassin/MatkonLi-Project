81cc96afbb887ecfad7f58a354f176221fc07a14
Matan Hassin <70580878+AnubisMatan@users.noreply.github.com>
2021-04-01 11:20:21 +0300

    connect action bar to nav

--- app/src/main/java/com/matanhassin/matkonli/HomeFragment.java
+++ app/src/main/java/com/matanhassin/matkonli/HomeFragment.java
@@ -1,23 +1,106 @@
 package com.matanhassin.matkonli;
-
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ImageButton;
 
 
 public class HomeFragment extends Fragment {
+
+    ImageButton spaguetti;
+    ImageButton steak;
+    ImageButton fish;
+    ImageButton cupcake;
+    ImageButton bread;
+    ImageButton pizza;
+    ImageButton salad;
+    ImageButton cheese;
+    ImageButton beer;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        spaguetti = view.findViewById(R.id.spaguetti);
+        steak= view.findViewById(R.id.steak);
+        fish= view.findViewById(R.id.fish);
+        cupcake= view.findViewById(R.id.cupcake);
+        bread= view.findViewById(R.id.bread);
+        pizza= view.findViewById(R.id.pizza);
+        salad= view.findViewById(R.id.salad);
+        cheese= view.findViewById(R.id.cheese);
+        beer= view.findViewById(R.id.beer);
+
+        spaguetti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HomeFragmentDirections.ActionHomeFragmentToListOfRecipesFragment action = HomeFragmentDirections.actionHomeFragmentToListOfRecipesFragment("spaguetti");
+                Navigation.findNavController(view).navigate(action);
+            }
+        });
+        steak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HomeFragmentDirections.ActionHomeFragmentToListOfRecipesFragment action = HomeFragmentDirections.actionHomeFragmentToListOfRecipesFragment("steak");
+                Navigation.findNavController(view).navigate(action);
+            }
+        });
+        fish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HomeFragmentDirections.ActionHomeFragmentToListOfRecipesFragment action = HomeFragmentDirections.actionHomeFragmentToListOfRecipesFragment("fish");
+                Navigation.findNavController(view).navigate(action);
+            }
+        });
+        cupcake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HomeFragmentDirections.ActionHomeFragmentToListOfRecipesFragment action = HomeFragmentDirections.actionHomeFragmentToListOfRecipesFragment("cupcake");
+                Navigation.findNavController(view).navigate(action);
+            }
+        });
+        bread.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HomeFragmentDirections.ActionHomeFragmentToListOfRecipesFragment action = HomeFragmentDirections.actionHomeFragmentToListOfRecipesFragment("bread");
+                Navigation.findNavController(view).navigate(action);
+            }
+        });
+        pizza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HomeFragmentDirections.ActionHomeFragmentToListOfRecipesFragment action = HomeFragmentDirections.actionHomeFragmentToListOfRecipesFragment("pizza");
+                Navigation.findNavController(view).navigate(action);
+            }
+        });
+        salad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HomeFragmentDirections.ActionHomeFragmentToListOfRecipesFragment action = HomeFragmentDirections.actionHomeFragmentToListOfRecipesFragment("salad");
+                Navigation.findNavController(view).navigate(action);
+            }
+        });
+        cheese.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HomeFragmentDirections.ActionHomeFragmentToListOfRecipesFragment action = HomeFragmentDirections.actionHomeFragmentToListOfRecipesFragment("cheese");
+                Navigation.findNavController(view).navigate(action);
+            }
+        });
+        beer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HomeFragmentDirections.ActionHomeFragmentToListOfRecipesFragment action = HomeFragmentDirections.actionHomeFragmentToListOfRecipesFragment("beer");
+                Navigation.findNavController(view).navigate(action);
+            }
+        });
+
         return view;
     }
 }
--- app/src/main/java/com/matanhassin/matkonli/MainActivity.java
+++ app/src/main/java/com/matanhassin/matkonli/MainActivity.java
@@ -1,14 +1,36 @@
 package com.matanhassin.matkonli;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    NavController navController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navController = Navigation.findNavController(this, R.id.mainactivity_navhost);
+        NavigationUI.setupActionBarWithNavController(this,navController);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == android.R.id.home)
+        {
+            navController.navigateUp();
+            return true;
+        }
+        else
+        {
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
