46adc24757c12cc04d370cfd08efc0be6b28e8c4
Matan Hassin <70580878+AnubisMatan@users.noreply.github.com>
2021-04-05 18:32:28 +0300

    add login and register design, intent between them,

--- app/src/main/java/com/matanhassin/matkonli/activities/login_page.java
+++ app/src/main/java/com/matanhassin/matkonli/activities/login_page.java
@@ -2,15 +2,37 @@ package com.matanhassin.matkonli.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.matanhassin.matkonli.R;
 
 public class login_page extends AppCompatActivity {
 
+    EditText emailInput;
+    EditText passwordInput;
+    Button loginBtn;
+    Button registerBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        registerBtn = findViewById(R.id.login_Page_Register_btn);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toRegisterPage();
+            }
+        });
+
+    }
+
+    private void toRegisterPage() {
+        Intent intent = new Intent(this, register_page.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/matanhassin/matkonli/activities/register_page.java
+++ app/src/main/java/com/matanhassin/matkonli/activities/register_page.java
@@ -2,15 +2,34 @@ package com.matanhassin.matkonli.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.matanhassin.matkonli.R;
 
 public class register_page extends AppCompatActivity {
 
+    Button loginBtn;
+    Button registerBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
+        loginBtn = findViewById(R.id.register_Page_Login_btn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toLoginPage();
+            }
+        });
+
+    }
+
+    private void toLoginPage() {
+        Intent intent = new Intent(this, login_page.class);
+        startActivity(intent);
     }
 }
