c3364bdab576cf634067c07d8c280ed0c2ce3642
Matan Hassin <70580878+AnubisMatan@users.noreply.github.com>
2021-04-05 10:14:07 +0300

    add firebase setup, add google-service, add dependencies and default config.

--- app/src/main/java/com/matanhassin/matkonli/model/ModelFirebase.java
+++ app/src/main/java/com/matanhassin/matkonli/model/ModelFirebase.java
@@ -1,7 +1,40 @@
 package com.matanhassin.matkonli.model;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ModelFirebase {
 
     public void addRecipe(Recipe recipe, Model.Listener<Boolean> listener) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Map<String, Object> data = new HashMap<>();
+        data.put("first", "Ada");
+        data.put("last", "Lovelace");
+        data.put("born", 1815);
+
+// Add a new document with a generated ID
+        db.collection("test")
+                .add(data)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("TAG", "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("TAG", "Error adding document", e);
+                    }
+                });
     }
 }
