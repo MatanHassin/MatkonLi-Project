a94e467582650638b9334456d12a242a38b42b94
Matan Hassin <70580878+AnubisMatan@users.noreply.github.com>
2021-04-05 21:26:15 +0300

    add firebase dependencies

--- app/src/main/java/com/matanhassin/matkonli/activities/register_page.java
+++ app/src/main/java/com/matanhassin/matkonli/activities/register_page.java
@@ -3,14 +3,24 @@ package com.matanhassin.matkonli.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.matanhassin.matkonli.R;
 
 public class register_page extends AppCompatActivity {
 
+    TextView title;
+    EditText userName;
+    ImageView profileImageView;
+    EditText email;
+    EditText password;
+    Uri profileImageUri = null;
     Button loginBtn;
     Button registerBtn;
 
