3cd6b692b783f159a94f696459a33415bf965aba
Matan Hassin <70580878+AnubisMatan@users.noreply.github.com>
2021-04-05 22:14:30 +0300

    add register modelfirebase

--- app/src/main/java/com/matanhassin/matkonli/model/ModelFirebase.java
+++ app/src/main/java/com/matanhassin/matkonli/model/ModelFirebase.java
@@ -1,40 +1,126 @@
 package com.matanhassin.matkonli.model;
-
-import android.util.Log;
-
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.webkit.MimeTypeMap;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
-
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentReference;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.matanhassin.matkonli.MyApplication;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class ModelFirebase {
 
-    public void addRecipe(Recipe recipe, Model.Listener<Boolean> listener) {
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        Map<String, Object> data = new HashMap<>();
-        data.put("first", "Ada");
-        data.put("last", "Lovelace");
-        data.put("born", 1815);
-
-// Add a new document with a generated ID
-        db.collection("test")
-                .add(data)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d("TAG", "DocumentSnapshot added with ID: " + documentReference.getId());
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w("TAG", "Error adding document", e);
-                    }
-                });
+    public interface Listener<T>{
+        void onComplete();
+        void onFail();
     }
+
+    public static void registerUser(final String username, String password, final String email, final Uri imageUri, final Listener<Boolean> listener){
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+        if(firebaseAuth.getCurrentUser() != null){
+            firebaseAuth.signOut();
+        }
+        if(username!=null
+        && !username.equals("") && password !=null && !password.equals("")
+        && email != null && !email.equals("") && imageUri !=null){
+            firebaseAuth.createUserWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    Toast.makeText(MyApplication.context,"User registered", Toast.LENGTH_SHORT).show();
+                    CreateUser(username,email,imageUri);
+                    listener.onComplete();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(MyApplication.context,"Failed registering user",Toast.LENGTH_SHORT).show();
+                    listener.onFail();
+                }
+            });
+        }
+        else{
+            Toast.makeText(MyApplication.context,"You must fill in all of the fields and profile image",Toast.LENGTH_SHORT).show();
+            listener.onFail();
+        }
+    }
+
+    private static void CreateUser(final String username, final String email, Uri imageUri){
+        final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        final FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference("images");
+        if (imageUri != null){
+            String imageName = username + "." + getExtension(imageUri);
+            final StorageReference imageRef = storageReference.child(imageName);
+
+            UploadTask uploadTask = imageRef.putFile(imageUri);
+            uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                @Override
+                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                    if (!task.isSuccessful()){
+                        throw task.getException();
+                    }
+                    return imageRef.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if (task.isSuccessful()){
+
+                        Map<String,Object> data = new HashMap<>();
+                        data.put("profileImageUrl", task.getResult().toString());
+                        data.put("username", username);
+                        data.put("email", email);
+                        data.put("info", "NA");
+                        firebaseFirestore.collection("userData").document(email).set(data).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void avoid) {
+                                if (firebaseAuth.getCurrentUser() != null){
+                                    firebaseAuth.signOut();
+                                }
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(MyApplication.context, "Fails to create user and upload data: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                    else if (!task.isSuccessful()){
+                        Toast.makeText(MyApplication.context, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+        else {
+            Toast.makeText(MyApplication.context, "Please choose a profile image", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public static String getExtension(Uri uri)
+    {
+        try{
+            ContentResolver contentResolver = MyApplication.context.getContentResolver();
+            MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+            return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+
+        } catch (Exception e) {
+            Toast.makeText(MyApplication.context, "Register page: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            return null;
+        }
+    }
+
+    public void addRecipe(Recipe recipe, Model.Listener<Boolean> listener) { }
 }
