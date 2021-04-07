3ff5427d8cfcd9e976b1ddc8ae8e8e74dfe1deb4
Matan Hassin <70580878+AnubisMatan@users.noreply.github.com>
2021-04-07 21:37:48 +0300

    finish with the profile page

--- app/src/main/java/com/matanhassin/matkonli/fragments/Profile_Fragment.java
+++ app/src/main/java/com/matanhassin/matkonli/fragments/Profile_Fragment.java
@@ -1,18 +1,24 @@
 package com.matanhassin.matkonli.fragments;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-
+import androidx.navigation.NavController;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import com.google.firebase.auth.FirebaseAuth;
 import com.matanhassin.matkonli.R;
-
+import com.matanhassin.matkonli.activities.login_page;
+import com.matanhassin.matkonli.model.User;
+import com.squareup.picasso.Picasso;
 
 public class Profile_Fragment extends Fragment {
 
@@ -39,6 +45,57 @@ public class Profile_Fragment extends Fragment {
         editProfileBtn = view.findViewById(R.id.profile_page_edit_profile_btn);
         myRecipesBook = view.findViewById(R.id.profile_page_my_recipes_book_btn);
         logoutBtn = view.findViewById(R.id.profile_page_logout_btn);
+        editProfileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditProfilePage();
+            }
+        });
+
+        myRecipesBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Profile_FragmentDirections.ActionProfileFragmentToMyRecipesListFragment action = Profile_FragmentDirections.actionProfileFragmentToMyRecipesListFragment(User.getInstance().userId);
+                Navigation.findNavController(view).navigate(action);
+            }
+        });
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logoutPage();
+            }
+        });
+
+        setUserProfile();
+
         return view;
+    }
+
+    private void setUserProfile() {
+        userName.setText(User.getInstance().username);
+        userEmail.setText(User.getInstance().userEmail);
+
+        if (User.getInstance().userprofileImageUrl != null)
+        {
+            Picasso.get().load(User.getInstance().userprofileImageUrl).noPlaceholder().into(userProfileImage);
+        }
+    }
+
+    private void logoutPage() {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(this.getActivity(), login_page.class));
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+    }
+
+    private void EditProfilePage()
+    {
+        NavController navCtrl = Navigation.findNavController(getActivity(), R.id.mainactivity_navhost);
+        NavDirections directions = Profile_FragmentDirections.actionProfileFragmentToEditProfileFragment();
+        navCtrl.navigate(directions);
     }
 }
